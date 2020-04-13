0c65b463ae980073b7d0fb5e358efc6f8a2d8e9a
Michel Kremer <kremi151dev@gmail.com>
2020-04-13 19:06:41 +0200

    Improved implementation for require imports

--- src/index.ts
+++ src/index.ts
@@ -14,25 +14,13 @@ interface Options {
     mappings: { [module: string]: TargetPath };
 }
 
-type ImportWriter = (imports: string, module: string) => string;
+type ImportWriter = (module: string, imports?: string) => string;
 
-function pipeGenericRequire(keyword: string, imports: string, module: string): string {
-    return `${keyword} ${imports} = require('${module}');`;
+function pipeRequire(module: string): string {
+    return `require('${module}')`;
 }
 
-function pipeLetRequire(imports: string, module: string) {
-    return pipeGenericRequire('let', imports, module);
-}
-
-function pipeConstRequire(imports: string, module: string) {
-    return pipeGenericRequire('const', imports, module);
-}
-
-function pipeVarRequire(imports: string, module: string) {
-    return pipeGenericRequire('var', imports, module);
-}
-
-function pipeImport(imports: string, module: string) {
+function pipeImport(module: string, imports: string | undefined) {
     return `import ${imports} from '${module}';`;
 }
 
@@ -56,31 +44,17 @@ export = function (options: Options) {
         }
         let inContent = String(file.contents);
         let outContent = inContent;
-        let regex = /(?:import(.*?)from ?(?:(?:"([^"]+)")|(?:'([^']+)'));?)|(?:(const|let|var).+?= ?require\((?:(?:"([^"]+)")|(?:'([^']+)'))\);?)/g;
+        let regex = /(?:import(.*?)from ?(?:(?:"([^"]+)")|(?:'([^']+)'));?)|(?:require\((?:(?:"([^"]+)")|(?:'([^']+)'))\))/g;
         let match;
         while ((match = regex.exec(inContent))) {
             let importWriter!: ImportWriter;
             let srcModule!: string;
-            let imports!: string;
+            let imports: string | undefined;
 
-            if (match[6] || match[7]) {
+            if (match[4] || match[5]) {
                 // require
-                if (!match[4] || !match[5]) {
-                    // TODO: What to do here?
-                    continue;
-                }
-                let keyword = match[4];
-                imports = match[5].trim();
-                srcModule = match[6] || match[7];
-                if (keyword === 'let') {
-                    importWriter = pipeLetRequire;
-                } else if (keyword === 'const') {
-                    importWriter = pipeConstRequire;
-                } else if (keyword === 'var') {
-                    importWriter = pipeVarRequire;
-                } else {
-                    return cb(new PluginError(PLUGIN_NAME, `Encountered invalid import in file ${file.basename}: ${match[0]}`));
-                }
+                srcModule = match[4] || match[5];
+                importWriter = pipeRequire;
             } else if (match[2] || match[3]) {
                 // import
                 imports = match[1].trim();
@@ -99,7 +73,7 @@ export = function (options: Options) {
                 destModule = resolveRelativePath(destModule.path, !!destModule.relative, file);
             }
 
-            const rewrittenImport = importWriter(imports, destModule);
+            const rewrittenImport = importWriter(destModule, imports);
             outContent = outContent.replace(match[0], rewrittenImport);
         }
 
